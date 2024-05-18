23870b3c44ba48ddb7fd29750048cc2791edba1c
Vitaliy K <vitaliyx@gmail.com>
2024-05-18 20:58:50 +0300

    Currency converter

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 
 
-const btnCollection = document.querySelectorAll('button')
-const img = document.querySelector('.img')
-
-for (const i of btnCollection) {
-    i.addEventListener('click',clickHandler)
-    
+const form = document.querySelector('form')
+const resultField = document.querySelector('#resultField');
+const currencyInput = document.querySelector('#currencyInput');
+const currencyOutput = document.querySelector('#currencyOutput');
+const KOEFFICIENT ={
+    USD: 36.6,
+    EUR: 40,
+    PLN:8,
+    UAN:1
 }
 
-function clickHandler ({target, target:{parentNode, dataset:{src}}}){
-   console.dir(target)
+form.addEventListener('submit',submitHandler )
+
+function submitHandler(event){
+
+    event.preventDefault();
+    const{  target:{amount: {value:amount}, currencyInput:{value: currencyInput},currencyOutput:{value: currencyOutput}}} = event;
+
+
+    if (Number.isNaN (Number(amount))){
+        resultField.textContent = 'Amount must be a number';
+    } else {
+        resultField.textContent = (amount* KOEFFICIENT[currencyInput]/ KOEFFICIENT[currencyOutput]).toFixed(2)
+    }
     
-    img.setAttribute('src', src)
- 
+
 
 }
