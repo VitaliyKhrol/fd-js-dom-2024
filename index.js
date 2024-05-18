29e5110d97e6caafc4403a4f96b80129c7adf6a3
Vitaliy K <vitaliyx@gmail.com>
2024-05-18 11:39:58 +0300

    Mouse

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 
-const h1 = document.querySelector('h1');
-h1.textContent = 'THE TEXT'
+const [btn1,btn2] = document.querySelectorAll('button')
+
+btn1.addEventListener('mouseover',switcer);
+btn2.addEventListener('mouseover',switcer);
+
+btn1.addEventListener('mouseout',switcer);
+btn2.addEventListener('mouseout',switcer);
+
+
+
+function switcer(){
+    let tmp = btn1.textContent;
+    btn1.innerText= btn2.textContent;
+    btn2.innerText =tmp;
+
+   
+}
