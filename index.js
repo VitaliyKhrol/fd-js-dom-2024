e3402641032837aaf5b4f9d5a2ce6951d0131426
Vitaliy K <vitaliyx@gmail.com>
2024-05-19 17:36:06 +0300

    classList example

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 
 
-const form = document.querySelector('form')
-const resultField = document.querySelector('#resultField');
-const currencyInput = document.querySelector('#currencyInput');
-const currencyOutput = document.querySelector('#currencyOutput');
-const KOEFFICIENT ={
-    USD: 36.6,
-    EUR: 40,
-    PLN:8,
-    UAN:1
-}
-
-form.addEventListener('submit',submitHandler )
-
-function submitHandler(event){
-
-    event.preventDefault();
-    const{  target:{amount: {value:amount}, currencyInput:{value: currencyInput},currencyOutput:{value: currencyOutput}}} = event;
+const root  = document.querySelector('#root');
+const toggler  = document.querySelector('.toggler');
 
 
-    if (Number.isNaN (Number(amount))){
-        resultField.textContent = 'Amount must be a number';
-    } else {
-        resultField.textContent = (amount* KOEFFICIENT[currencyInput]/ KOEFFICIENT[currencyOutput]).toFixed(2)
-    }
-    
+const lamp =document.createElement('div');
+lamp.classList.add('lamp-off');
+
+root.append(lamp)
 
 
+
+toggler.addEventListener('click',toggle)
+
+function toggle(){
+    lamp.classList.toggle('lamp-off');
+    lamp.classList.toggle('lamp-on');
+    console.log(lamp.classList);
 }
