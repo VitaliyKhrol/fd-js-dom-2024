55ec29ec7f595e4489d4d820d4efee5897c4d012
Vitaliy K <vitaliyx@gmail.com>
2024-05-19 18:35:04 +0300

    Article structure

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 
 
+// const root  = document.querySelector('#root');
+// const toggler  = document.createElement('button');
+// toggler.textContent = 'Switch';
+// document.body.append(toggler);
+
+
+// const lamp =document.createElement('div');
+// lamp.classList.add('lamp-off');
+
+// root.append(lamp)
+
+
+
+// toggler.addEventListener('click',toggle)
+
+// function toggle(){
+//     lamp.classList.toggle('lamp-off');
+//     lamp.classList.toggle('lamp-on');
+//     console.log(lamp.classList);
+// }
+
+
+
 const root  = document.querySelector('#root');
-const toggler  = document.querySelector('.toggler');
+
+const article =document.createElement('article');
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
 
 
-const lamp =document.createElement('div');
-lamp.classList.add('lamp-off');
-
-root.append(lamp)
+h1.textContent='Header';
+p.textContent= 'TEXT';
 
 
-
-toggler.addEventListener('click',toggle)
-
-function toggle(){
-    lamp.classList.toggle('lamp-off');
-    lamp.classList.toggle('lamp-on');
-    console.log(lamp.classList);
-}
+article.append(h1,p)
+root.append(article)
