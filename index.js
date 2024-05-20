32bd0ea3a91a154005b9487c2ff012289dd6904c
Vitaliy K <vitaliyx@gmail.com>
2024-05-20 17:42:24 +0300

    Карточка юзера ФИНАЛ

--- index.js
+++ index.js
@@ -5,19 +5,15 @@ const cardArray = userData.map(user => createUserCard(user));
 
 
 function createUserCard(user) {
-    const img = document.createElement('img');
-    img.classList.add('avatar');
-    img.setAttribute('src', user.profilePicture);
-    img.setAttribute('alt', user.name)
-
+    const imageWrapper= createImageWrapper(user);
    
     const h2= crElement('h2',{}, user.name);
     const p =crElement('p',{},user.description);
     const button = crElement('button',{},'Continue')
     
-    const wrapper = crElement('div', {classNames: ['card-wrapper']},img, h2, p, button )
+    const wrapper = crElement('div', {classNames: ['card-wrapper']},imageWrapper, h2, p, button )
 
-        wrapper.addEventListener('click', getActiveCard);
+    wrapper.addEventListener('click', getActiveCard);
 
     return wrapper;
 }
@@ -25,7 +21,7 @@ function createUserCard(user) {
 
 
 function getActiveCard(event) {
-    console.dir(event.curentTarget)
+
     const activeCard = document.querySelector('.active');
     if (activeCard === event.curentTarget) {
         return
@@ -37,6 +33,8 @@ function getActiveCard(event) {
     event.curentTarget.classList.add('active')
 }
 
+
+
 /**
  * 
  * @param {String} type 
@@ -47,17 +45,47 @@ function getActiveCard(event) {
  */
 
 
-
 function crElement(type,{classNames=[]}, ...children){
-
-    console.log(type);
-    console.log(classNames)
-    console.log(children)
-
     const elem = document.createElement(type);
     elem.classList.add(...classNames);
     elem.append(...children);
     return elem;
 }
+
+function createImageWrapper(user){
+    const imageWrapper = crElement('div',{classNames:['images-wrapper']},user.name[0]);
+    imageWrapper.setAttribute('id', `wrapper-${user.id}`)
+    const img = createImage(user);
+    return imageWrapper;
+}
+
+
+function createImage(user){
+
+    const img = document.createElement('img');
+  
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.dataset.id = user.id;
+    img.classList.add('avatar');
+    img.addEventListener('error',imageErrorHandler )
+    img.addEventListener('load', imageLoadHandler)
+    return img;
+}
+
+function imageLoadHandler(event){
+   
+    const parentWrapper = document.querySelector(`#wrapper-${event.target.dataset.id}`);
+    parentWrapper.append(event.target);
+    console.dir(event.target);
+
+}
+
+function imageErrorHandler(event){
+    event.target.remove();
+
+}
+
+
 
 root.append(...cardArray)
--- userData.js
+++ userData.js
@@ -3,7 +3,7 @@ const userData = [
         "id": 1,
         "name": "John",
         "description": "fafewqrewrqwer2322revzd",
-        "profilePicture": "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
+        "profilePicture": "https://mages.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
     },
     {
         "id": 2,
