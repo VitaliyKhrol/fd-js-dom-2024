ed51436a29e7bc30fbce0e8ad80526955b3d14e6
Vitaliy K <vitaliyx@gmail.com>
2024-05-20 11:52:57 +0300

    Карточка юзера

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 
-const root  = document.querySelector('#root');
-const array =[
-    './images/pexels-iriser-1381679.jpg',
-    './images/pexels-michael-block-1691617-3225517.jpg',
-    './images/pexels-pixabay-53125.jpg',
-    './images/pexels-pixabay-236599.jpg'
-]
+const root = document.querySelector('#root');
+
+const cardArray = userData.map(user => createUserCard(user));
 
 
-function createImage(imgScr){
-const img = document.createElement('img');
-img.setAttribute('src',imgScr);
-img.classList.add('img')
-return img;
+function createUserCard(user) {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('card-wrapper');
+
+    const img = document.createElement('img');
+    img.classList.add('avatar');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name)
+
+    const h2 = document.createElement('h2');
+    h2.append(user.name);
+
+    const p = document.createElement('p');
+    p.append(user.description);
+
+    const button = document.createElement('button');
+    button.append('Content')
+
+    wrapper.append(img, h2, p, button);
+
+    wrapper.addEventListener('click', getActiveCard);
+
+    return wrapper;
 }
 
-const imageArray = array.map(createImage)
 
-root.append(...imageArray )
+
+function getActiveCard(event) {
+    console.dir(event.curentTarget)
+    const activeCard = document.querySelector('.active');
+    if (activeCard === event.curentTarget) {
+        return
+    } else
+        if (activeCard) {
+            activeCard.classList.remove('active');
+        }
+
+    event.curentTarget.classList.add('active')
+}
+
+
+root.append(...cardArray)
